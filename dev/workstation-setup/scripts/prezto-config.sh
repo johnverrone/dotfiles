16138f7dc7454ae3af82a255c488b910a5a0c728
Verrone <jxv5520@c02w10k9j1gqim.amer.homedepot.com>
2018-08-06 10:40:51 -0400

    removing workstation-setup

--- dev/workstation-setup/scripts/prezto-config.sh
+++
@@ -1,8 +0,0 @@
-echo
-echo "Configuring prezto"
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" || true
-
-echo "setting zsh as default shell"
-chsh -s /bin/zsh
-
