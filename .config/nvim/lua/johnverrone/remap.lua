3a695d56be862adf66a9e1c61db108ed99a4f890
John Verrone <johnverrone@fullstory.com>
2024-01-05 09:43:35 -0700

    remap changes

--- .config/nvim/lua/johnverrone/remap.lua
+++ .config/nvim/lua/johnverrone/remap.lua
@@ -1,12 +1,29 @@
 -- go back to net-rw easily
 vim.keymap.set("n", "-", vim.cmd.Ex)
 
+-- make joining lines not jump to end
+vim.keymap.set("n", "J", "mzJ`z")
+
 -- keep searches in the middle
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- paste in visual and keep pasted value in register
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- faster saving
 vim.keymap.set("n", "zz", vim.cmd.update)
+
+-- yank to clipboard with leader-y/Y
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- keep cursor in place when page up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- setup substitute with value under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- easy open nvim plugins
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/johnverrone/plug.lua<CR>")
