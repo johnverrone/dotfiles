de914c49dcaf3afc95abbbe1a5eadca5ecf0f40f
John Verrone <johnverrone@fullstory.com>
2024-01-09 14:02:52 -0700

    vim-fugitive

--- .config/nvim/lua/johnverrone/plug.lua
+++ .config/nvim/lua/johnverrone/plug.lua
@@ -1,32 +1,35 @@
-local Plug = vim.fn['plug#']
+local Plug = vim.fn["plug#"]
 
-vim.call('plug#begin')
+vim.call("plug#begin")
 
 -- treesitter for better syntax highlighting
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
 
--- telescope fuzzy finder 
-Plug 'nvim-lua/plenary.nvim'
-Plug('nvim-telescope/telescope.nvim', {tag = '0.1.5'})
+-- telescope fuzzy finder
+Plug("nvim-lua/plenary.nvim")
+Plug("nvim-telescope/telescope.nvim", { tag = "0.1.5" })
 
 -- colorscheme
-Plug 'tomasiser/vim-code-dark'
+Plug("tomasiser/vim-code-dark")
 
 -- lsp and autocompletion
-Plug 'neovim/nvim-lspconfig'
-Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug('L3MON4D3/LuaSnip', {tag = 'v2.*', ['do'] = 'make install_jsregexp'})
+Plug("neovim/nvim-lspconfig")
+Plug("williamboman/mason.nvim")
+Plug("williamboman/mason-lspconfig.nvim")
+Plug("hrsh7th/cmp-nvim-lsp")
+Plug("hrsh7th/cmp-buffer")
+Plug("hrsh7th/cmp-path")
+Plug("hrsh7th/cmp-cmdline")
+Plug("hrsh7th/nvim-cmp")
+Plug("L3MON4D3/LuaSnip", { tag = "v2.*", ["do"] = "make install_jsregexp" })
 
 -- golang
-Plug('fatih/vim-go', {['do'] = ':GoUpdateBinaries' })
+Plug("fatih/vim-go", { ["do"] = ":GoUpdateBinaries" })
 
 -- run formatting tools
-Plug 'mhartington/formatter.nvim'
+Plug("mhartington/formatter.nvim")
 
-vim.call('plug#end')
+-- git
+Plug("tpope/vim-fugitive")
+
+vim.call("plug#end")
