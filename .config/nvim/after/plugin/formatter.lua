e364b0a9568e406ce185f3d2c36f99b580af5ce7
John Verrone <johnverrone@fullstory.com>
2024-01-09 13:12:59 -0700

    css

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -1,25 +1,27 @@
 -- Utilities for creating configurations
-local util = require "formatter.util"
+local util = require("formatter.util")
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  filetype = {
-    lua = { require("formatter.filetypes.lua").stylua },
-    svelte = { require("formatter.filetypes.svelte").prettier },
-    typescript = { require("formatter.filetypes.typescript").prettier },
-    typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
+require("formatter").setup({
+	filetype = {
+		css = { require("formatter.filetypes.css").prettier },
+		lua = { require("formatter.filetypes.lua").stylua },
+		scss = { require("formatter.filetypes.css").prettier },
+		svelte = { require("formatter.filetypes.svelte").prettier },
+		typescript = { require("formatter.filetypes.typescript").prettier },
+		typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
 
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace
-    }
-  }
-}
+		-- Use the special "*" filetype for defining formatter configurations on
+		-- any filetype
+		["*"] = {
+			-- "formatter.filetypes.any" defines default configurations for any
+			-- filetype
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
+})
 
-vim.keymap.set('n', '<leader>F', vim.cmd.Format)
+vim.keymap.set("n", "<leader>F", vim.cmd.Format)
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
