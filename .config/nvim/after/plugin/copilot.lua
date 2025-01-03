0b2ac43de7aa5a66e17dfe2ee487b54196d1b89f
John Verrone <johnverrone@fullstory.com>
2025-01-03 14:28:40 -0700

    copilot chat model

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,3 +1,17 @@
 require("CopilotChat").setup({
 	-- See Configuration section for options
+	model = "claude-3.5-sonnet",
 })
+
+-- show prompts with telescopt
+vim.keymap.set("n", "<leader>ccp", function()
+	local actions = require("CopilotChat.actions")
+	require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+end)
+
+vim.keymap.set("n", "<leader>ccq", function()
+	local input = vim.fn.input("Chat > ")
+	if input ~= "" then
+		require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+	end
+end)
