5732e2e8c9349ea774a03ad611db00848d3ca835
John Verrone <johnverrone@fullstory.com>
2025-02-20 13:09:12 -0700

    remove fugitive remap and updated copilots keymaps

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -8,15 +8,18 @@ require("copilot_cmp").setup()
 require("CopilotChat").setup({
 	-- See Configuration section for options
 	model = "claude-3.5-sonnet",
+	window = {
+		width = 0.4,
+	},
 })
 
 -- show prompts with telescope
-vim.keymap.set("n", "<leader>ccp", function()
+vim.keymap.set("n", "<leader>cp", function()
 	local actions = require("CopilotChat.actions")
 	require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
 end)
 
-vim.keymap.set("n", "<leader>ccq", function()
+vim.keymap.set("n", "<leader>cq", function()
 	local input = vim.fn.input("Chat > ")
 	if input ~= "" then
 		require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
