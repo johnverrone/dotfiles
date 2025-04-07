96665de4cc3a5ea608f1b4b11a7f23f19a029911
John Verrone <johnverrone@fullstory.com>
2025-04-07 16:30:23 -0600

    fix tmux url launcher

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,9 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  opts = {
-    model = "claude-3.5-sonnet",
-  },
+  "zbirenbaum/copilot.lua",
+  optional = true,
+  opts = function()
+    -- Temporary fix remove after https://github.com/LazyVim/LazyVim/pull/5900 is merged
+    ---@diagnostic disable-next-line: inject-field
+    require("copilot.api").status = require("copilot.status")
+  end,
 }
