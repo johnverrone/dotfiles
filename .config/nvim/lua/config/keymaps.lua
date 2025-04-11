624b5e591cbb0c7f3eb078756314b73234c383c2
John Verrone <johnverrone@fullstory.com>
2025-04-11 13:12:29 -0600

    more keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- delete defaults
 vim.keymap.del("n", "<leader>gB")
-
--- Add any additional keymaps here
--- make joining lines not jump to end
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
 -- paste in visual and keep pasted value in register
 vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- faster saving
 vim.keymap.set("n", "zz", vim.cmd.update)
+
+-- yank to clipboard with leader-y/Y
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
